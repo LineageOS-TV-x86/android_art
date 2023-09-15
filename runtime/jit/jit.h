a2583a24d08c6fe89704c98a2470f3e20a0aefc4
Reddy, Alavala Srinivasa <alavala.srinivasa.reddy@intel.com>
2023-09-15 19:17:55 +0530

    Auto Fast JNI Optimization
    Change-Id: I87c210b0ee40370ad4884be7c5baafe7a995c331
    Signed-off-by: Reddy, Alavala Srinivasa <alavala.srinivasa.reddy@intel.com>

--- runtime/jit/jit.h
+++ runtime/jit/jit.h
@@ -58,6 +58,7 @@ class JitCodeCache;
 class JitCompileTask;
 class JitMemoryRegion;
 class JitOptions;
+class JniTask : public Task { };
 
 static constexpr int16_t kJitCheckForOSR = -1;
 static constexpr int16_t kJitHotnessDisabled = -2;
@@ -317,7 +318,10 @@ class Jit {
   JitThreadPool* GetThreadPool() const {
     return thread_pool_.get();
   }
-
+  
+  
+  bool AddJniTask(Thread* self, JniTask* task);
+  
   // Stop the JIT by waiting for all current compilations and enqueued compilations to finish.
   EXPORT void Stop();
 
--- runtime/runtime.h
+++ runtime/runtime.h
@@ -970,6 +970,16 @@ class Runtime {
   char** GetEnvSnapshot() const {
     return env_snapshot_.GetSnapshot();
   }
+  
+  // Should Auto fast Detection be done.
+  bool IsAutoFastDetect() const {
+    return auto_fast_detect_;
+  }
+
+  // Set Auto Fast Detection.
+  void SetAutoFastDetect(bool value) {
+    auto_fast_detect_ = value;
+  }  
 
   EXPORT void AddSystemWeakHolder(gc::AbstractSystemWeakHolder* holder);
   EXPORT void RemoveSystemWeakHolder(gc::AbstractSystemWeakHolder* holder);
@@ -1576,6 +1586,9 @@ class Runtime {
       static_cast<uint32_t>(DeoptimizationKind::kLast) + 1];
 
   MemMap protected_fault_page_;
+  
+  // Auto Fast JNI detection gate.
+  bool auto_fast_detect_;
 
   uint32_t verifier_logging_threshold_ms_;
 
